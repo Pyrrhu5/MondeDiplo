51010294a990ac6eceaca2e95f5f0dcb4dde2b3a
Aymeric Deschard <aymeric.deschard@disroot.org>
2020-09-12 14:17:10 +0200

    Full process uncommented

--- MondeDiplo.py
+++ MondeDiplo.py
@@ -11,13 +11,13 @@ MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == "__main__":
 	CONFIG = Config(MODULE_PATH)
-	# conn = Scraper(CONFIG.diploUrl, os.path.join(MODULE_PATH, "failed"))
-	# conn.connect(CONFIG.diploUsername, CONFIG.diploPassword)
-	# ebookPath = conn.download_latest_edition(FileFormat.epub)
-	# if ebook: print(f"Successfully downloaded ebook: {ebookPath}")
+	conn = Scraper(CONFIG.diploUrl, os.path.join(MODULE_PATH, "failed"))
+	conn.connect(CONFIG.diploUsername, CONFIG.diploPassword)
+	ebookPath = conn.download_latest_edition(FileFormat.epub)
+	if ebookPath: print(f"Successfully downloaded ebook: {ebookPath}")
 	calibre = Calibre(
 		CONFIG.calibreUrl,
 		CONFIG.calibreUsername,
 		CONFIG.calibrePassword
 	)
-	print(calibre.add_to_library("/tmp/6c883c3399257a46.epub"))
+	calibre.add_to_library(ebookPath)
